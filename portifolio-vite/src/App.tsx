da82b86dd4ba13b80b1e99aa0d424415b8b57295
Thr0ughTheFloor <contatofabio2005@gmail.com>
2025-09-22 14:47:02 -0300

    Base APP.TSX + criação assets/images + exportação das imagens

--- portifolio-vite/src/App.tsx
+++ portifolio-vite/src/App.tsx
@@ -1,12 +1,21 @@
-import { useState } from 'react'
 import './App.css'
 
 function App() {
-  
+
    return (
-    <>
-      
-    </>
+    // Desenvolver Menu
+    <main className='color '>
+      {/* Sessão do Banner */}
+      <section>
+        <div>
+
+        </div>
+      </section>
+      {/* Sessão do resto da Page */}
+      <section></section>
+      {/* Sessão do Footer/Contatos */}
+      <section></section>
+    </main>
   )
 }
 
