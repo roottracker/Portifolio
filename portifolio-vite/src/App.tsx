f90ad4f6c47531f4c45c4c128ab831774eeb5ea3
Thr0ughTheFloor <contatofabio2005@gmail.com>
2025-09-27 16:32:46 -0300

    add components & info + css

--- portifolio-vite/src/App.tsx
+++ portifolio-vite/src/App.tsx
@@ -1,13 +1,45 @@
 import './App.css'
 import face1 from './assets/images/face1.png'
 import face2 from './assets/images/face2.png'
-import { QuemSou, Gostos } from './components/intro'
+import pfp from "./assets/images/Calendula-1.png";
+import { QuemSou} from './components/intro'
+import { Info3 } from './components/conhecimentos'
+import { useEffect, useState } from 'react'
+
 
 function App() {
 
+  const [showPfp, setShowPfp] = useState(false)
+
+    useEffect(() => {
+    const handleEsc = (event: KeyboardEvent) => {
+      if (event.key === "Escape") {
+        setShowPfp(false);
+      }
+    };
+
+    document.addEventListener("keydown", handleEsc);
+
+    
+    return () => {
+      document.removeEventListener("keydown", handleEsc);
+    };
+  }, []);
+
    return (
     // Desenvolver Menu
+
     <main className=''>
+      {/* Mostrar Foto Modal */}
+        {showPfp && (
+          <div  className={`fixed inset-0 flex items-center justify-center z-[9999] transition-opacity duration-500`}>
+            <div className='max-w-150'>
+              <button onClick={() => setShowPfp(prev => !prev)} className='absolute'>✖</button>
+              <img src={pfp} alt="" className='w-150' />
+            </div>
+          </div>          
+        )}
+      
       {/* Sessão do Banner */}
       <section>
         <div>
@@ -15,7 +47,7 @@ function App() {
         </div>
       </section>
       {/* Sessão do resto da Page */}
-      <section className='relative h-full w-full flex justify-start items-center flex-col bg-transparent overflow-visible before:absolute before:content-[""] before:inset-0 before:bg-[url(assets/images/background-img.jpg)] before:bg-repeat before:bg-cover before:opacity-[0.5] before:z-[-1] before:bg-center before:lg:bg-top'>
+      <section className={`relative h-full w-full flex justify-start items-center flex-col bg-transparent overflow-visible before:absolute before:content-[""] before:inset-0 before:bg-[url(assets/images/background-img.jpg)] before:bg-repeat before:bg-cover before:opacity-[0.5] before:z-[-1] before:bg-center before:lg:bg-top ${showPfp ? "brightness-20" : "brightness-100"}`}>
         
         {/* 1ª Imagem Separadora */}
         <div className='flex justify-center mt-50'>
@@ -23,15 +55,19 @@ function App() {
         </div>
 
         {/* Introdução Conteiner */}
-        <div>
-          <QuemSou/>
-          <img draggable={false} src={face2} alt="" className='static select-none top-[20] left-[20] w-40 pointer-none z-[9999px] invert'/>
-          <Gostos/>
+        <div className='flex justify-center items-center flex-col gap-[7rem]'>
+            <div className='flex flex-col gap-[5rem] mt-20 xl:flex-row xl:gap-[10rem]'>
+              <div className="relative w-full max-w-100 m-auto">
+                  <img onClick={() => setShowPfp(prev => !prev)} src={pfp} alt="Foto de perfil" className="w-full h-auto rounded-full duration-1000 ease-in-out cursor-pointer hover:scale-[1.03]"/>
+              </div>
+              <QuemSou/>
+            </div>
+          <img draggable={false} src={face2} alt="" className='static select-none top-[10] left-[20] w-40 pointer-none z-[9999px] invert'/>
         </div>
 
-        <div>
-          <h3>Conhecimento</h3>
-          {/* info3 */}
+        <div className='flex justify-center items-center flex-col'>
+          <h3 className='mt-[50px] text-white font-[500] tracking-[10px] text-[18pt]'>Conhecimento</h3>
+          <Info3/>
           <div>
             <img src="" alt="" />
           </div>
--- portifolio-vite/src/components/conhecimentos.tsx
+++ portifolio-vite/src/components/conhecimentos.tsx
@@ -1,4 +1,35 @@
-export const info3 = () => {
-
+export const Info3 = () => {
+    return(
+        <section className="flex w-full items-center justify-center mt-20 gap-20 flex-col xl:flex-row">
+            <div className="flex max-w-130 w-full h-auto border border-white flex-col px-8">
+                <p className="mt-4 text-white font-medium tracking-[5px] text-[14pt] text-center font-mono">Como artista, eu gosto de me aventurar pela profundidade dos sentimentos e sensações humanas, 
+            principalmente a melancolia, o pânico, a solidão e o vazio, tentando expor os diferentes “subgêneros” 
+            desses sentimentos, amo textura e procuro sempre utilizá-la em todos os meus trabalhos artísticos.</p>
+                <br />
+                <p className="mt-5 text-white font-medium tracking-[5px] text-[14pt] text-center font-mono">Como programador iniciante, gosto muito de back-end e linguagens de baixo nível, como C, e de alto nível como Java. 
+            Gosto de desafios, procuro estar me habituando e sempre atento ao complexo e imparável mundo da tecnologia.</p>
+                <br />
+            </div>
+            <div className="block max-w-130 max-h-200 w-full">
+                <h4 className="text-white font-bold font-mono text-center w-full max-w-200 mb-[2rem] ml-[10%] font-[14pt]">Produção musical e mixagem de áudio</h4>
+                <p className="text-white font-mono text-center w-full max-w-200 mb-[3rem] ml-[10%] font-[14pt]">[██████████████-----] 75%</p>
+                <br />
+                <h4 className="text-white font-bold font-mono text-center w-full max-w-200 mb-[1rem] ml-[10%] font-[14pt]">Edição de imagem e vídeo</h4>
+                <p className="text-white font-mono text-center w-full max-w-200 mb-[3rem] ml-[10%] font-[14pt]">[██████--------------] 40%</p>
+                <br />
+                <h4 className="text-white font-bold font-mono text-center w-full max-w-200 mb-[1rem] ml-[10%] font-[14pt]">Desenho</h4>
+                <p className="text-white font-mono text-center w-full max-w-200 mb-[3rem] ml-[10%] font-[14pt]">[████----------------] 30%</p>
+                <br />
+                <h4 className="text-white font-bold font-mono text-center w-full max-w-200 mb-[1rem] ml-[10%] font-[14pt]">Eletrônica</h4>
+                <p className="text-white font-mono text-center w-full max-w-200 mb-[3rem] ml-[10%] font-[14pt]">[██------------------] 10%</p>
+                <br />
+                <h4 className="text-white font-bold font-mono text-center w-full max-w-200 mb-[1rem] ml-[10%] font-[14pt]">Back-end</h4>
+                <p className="text-white font-mono text-center w-full max-w-200 mb-[3rem] ml-[10%] font-[14pt]">[███-----------------] 15%</p>
+                <br />
+                <h4 className="text-white font-bold font-mono text-center w-full max-w-200 mb-[1rem] ml-[10%] font-[14pt]">Front-end</h4>
+                <p className="text-white font-mono text-center w-full max-w-200 mb-[3rem] ml-[10%] font-[14pt]">[█-------------------] 7%</p><br />
+            </div>
+        </section>
+    )
 }
 
